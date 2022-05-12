8a974150a2e40d3995420fe1a4887c0cdb43b653
Will <willchesson@gmail.com>
2022-05-12 15:11:49 -0400

    commit tree e1ae204d3b83faaa8d40f0a48aa6fb04fc41194d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    array[-1, 0] = "and"
+    str = array.join(", ")
+
+    if str.include?("and,")
+        str = str.split(" ")
+    end
+    str[-2] = "and"
+    str.join(" ")
 end
+
+puts oxford_comma(["lion", "witch", "wardrobe"])
+
+#add and before last element in array (shoveling)
+#how to access last element of array -- array[-1]
+#use .split(",") to convert the string into an array
